dc443ed6d3730e1dfc022aef13192a850654d1df
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-03-18 10:50:19 -0500

    Add paper kit files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/scss/paper-kit.scss";
+import "assets/demo/demo.css";
+
+// pages
+import LandingPage from "views/examples/LandingPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Workorona is comming soon...</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" render={props => <LandingPage {...props} />} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
