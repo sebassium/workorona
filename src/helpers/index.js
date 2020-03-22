05d9972e19c7c93cfc952c90cc6a0ee32281e4b2
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-03-21 23:01:23 -0500

    Add all qr and links

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,14 +5,70 @@ export const type = code => {
         link:
           "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-c9d380b1-50c7-4216-bb66-cb6d11dd71e4",
         qr:
-          "https://res.cloudinary.com/sebashr20/image/upload/v1584841521/seed/qr_type1.jpg"
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584841521/seed/qrCodes/qr_type1.jpg"
       };
     case "type2": // $24,900
       return {
         link:
           "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-b102b9e2-33c1-4cc3-ad73-73ec8fc7265a",
         qr:
-          "https://res.cloudinary.com/sebashr20/image/upload/v1584841658/seed/qr_type2.jpg"
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584841658/seed/qrCodes/qr_type2.jpg"
+      };
+    case "type3": // $34,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-940f2daa-d115-48ca-9bd3-960a0b892569",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849266/seed/qrCodes/qr_type3.jpg"
+      };
+    case "type4": // $44,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-82e9cc13-d2fa-41fb-990e-da6c39a65b9b",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849307/seed/qrCodes/qr_type4.jpg"
+      };
+    case "type5": // $54,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-d32bfcc7-b191-4383-9b67-cb59f13e41e0",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849340/seed/qrCodes/qr_type5.jpg"
+      };
+    case "type6": // $64,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-f401f452-b7ab-43f7-9184-eb9608d4605b",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849351/seed/qrCodes/qr_type6.jpg"
+      };
+    case "type7": // $74,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-8974475a-142d-4189-a7b3-362fa33675d2",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849363/seed/qrCodes/qr_type7.jpg"
+      };
+    case "type8": // $84,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-3b4f6958-28b1-4134-80bb-9d337834a34e",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849375/seed/qrCodes/qr_type8.jpg"
+      };
+    case "type9": // $94,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-6f8ced09-6931-4093-be43-c29ed976fab0",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849387/seed/qrCodes/qr_type9.jpg"
+      };
+    case "type10": // $104,900
+      return {
+        link:
+          "https://www.mercadopago.com.co/checkout/v1/redirect?pref_id=151726621-ae8a53b5-3072-42cb-90eb-cc88607db9a2",
+        qr:
+          "https://res.cloudinary.com/sebashr20/image/upload/v1584849400/seed/qrCodes/qr_type10.jpg"
       };
     default:
       return {
